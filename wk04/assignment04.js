e68095aa84741d1d9b7f77df12f159aba5ad03b7
Ting <mycentry1986@gmail.com>
2019-03-02 18:01:07 -0500

    first commit

--- wk04/assignment04.js
+++ wk04/assignment04.js
@@ -5,19 +5,34 @@
 //img5 link: https://www.chinadiscovery.com/chengdu-tours/6-days-jiuzhaigou-chengdu-tour.html
 let slideIndex=1;
 let imageArray=document.getElementsByClassName("cImagSpan");
+var windowObjectReference = null; 
 
 function addEventAddFav()
 {
     let addBelowClick=document.getElementsByClassName("cImagSpanBelow");
+    let getCanvasNodes=document.getElementsByTagName("canvas");
+    let getCanvasSpanNodes=document.getElementsByClassName("cImagShowClass");
 
     for (let i=0;i<imageArray.length;i++)
     {
         let belowImgClickId=addBelowClick[i].getAttribute("id");
-        addBelowClick[i].addEventListener("click",function foo(){belowImgClick(belowImgClickId)});
+        let getCanvasId=getCanvasNodes[i].getAttribute("id");
+        let getCanvasSpanId=getCanvasSpanNodes[i].getAttribute("id");
+        if (window.addEventListener)
+        {
+            addBelowClick[i].addEventListener("click",function foo(){belowImgClick(belowImgClickId)});
+        }
+        else if(window.attachEvent)
+        {
+            addBelowClick[i].attachEvent("click",function foo(){belowImgClick(belowImgClickId)});
+        }
         imageArray[i].classList.add("addOnloadDislay");
+        drawHeart(getCanvasId);
+        HeartDisplayOnload(getCanvasSpanId);
     }
     onloadImgShow();
     midAddFev();
+    
 }
 
 function imageDisplayClick(num)
@@ -42,7 +57,7 @@ function imageDisplayClick(num)
 }
 function addImgFav()
 {
-    let idGet=getId().id;
+    let idGet=getMidNode().id;
     let item=document.getElementById(idGet);
     let favBan=document.getElementById("favImgBan");
     let copyNode=item.cloneNode(true);
@@ -52,10 +67,15 @@ function addImgFav()
     copyNode.classList.remove("cImagSpan");
     copyNode.classList.add("addFavSpanClass");
     copyNode.classList.remove("addOnloadStyleZDexTwo");
-
     let checkResult=checkDuplication(favBan,copyNode);
-   
     addFunctionToSingle(checkResult,copyNode,favBan);
+
+    let ZDexTwoElements=document.getElementsByClassName("addOnloadStyleZDexTwo");
+    let ParentNodeId=ZDexTwoElements[0].getAttribute("id");
+    let ParentNode=document.getElementById(ParentNodeId);
+    let childNodeList=ParentNode.querySelector("canvas");
+    let canvasNodeId=childNodeList.getAttribute("id");
+    fillHeart(canvasNodeId);
 
 }
 function removeImgFav(id)
@@ -63,6 +83,9 @@ function removeImgFav(id)
     let nodeChild=document.getElementById(id);
     let nodePar=document.getElementById("favImgBan");
     nodePar.removeChild(nodeChild);
+    let childNodeIdArray=id.split("-");
+    let canvasId=childNodeIdArray[0]+"-canvas";
+    removeFillHeart(canvasId);
 
 }
 function belowImgClick(id)
@@ -165,38 +188,76 @@ function onloadImgShow()
 }
 function midAddFev()
 {
-    let midImgShowDiv=getId();
- //   midImgShowDiv.addEventListener("click",addImgFav)
+    let midImgShowDiv=getMidNode();
+//    midImgShowDiv.addEventListener("click",addImgFav)
     let id =midImgShowDiv.id;
-    midImgShowDiv.addEventListener("click",function(){openNewWidow(id)});
-    
+    let midCavSpanId=id+"-canvas-span";
+    // midImgShowDiv.addEventListener("click",function(){openNewWidow(id)});
+    let midId=midImgShowDiv.getAttribute("id");
+    let canvasMidNode=getMidCanvasNode(midId);
+    if (window.addEventListener)
+    {
+        canvasMidNode.addEventListener("click",addImgFav);
+        midImgShowDiv.addEventListener("mouseover",heartDisplayOnHover);
+        midImgShowDiv.addEventListener("mouseout",HeartDisplayMouseOut);
+    }
+    else if(window.attachEvent)
+    {
+        canvasMidNode.attachEvent("click",addImgFav);
+        midImgShowDiv.attachEvent("mouseover",heartDisplayOnHover);
+        midImgShowDiv.attachEvent("mouseout",HeartDisplayMouseOut);
+    }
+
 }
 function midAddFevFunctionRemove()
 {
     // let midImgShowDiv=document.getElementById("cImagShow");
     // midImgShowDiv.removeEventListener("click",function foo(){addImgFav(num)});
-    let midImgShowDiv=getId();
-    midImgShowDiv.removeEventListener("click",addImgFav)
+    let midImgShowDiv=getMidNode();
+    let midId=midImgShowDiv.getAttribute("id");
+    let canvasMidNode=getMidCanvasNode(midId);
+//    midImgShowDiv.removeEventListener("click",addImgFav);
+    if (window.removeEventListener)
+    {
+        canvasMidNode.removeEventListener("click",addImgFav);
+        midImgShowDiv.removeEventListener("mouseover",heartDisplayOnHover);
+        midImgShowDiv.removeEventListener("mouseout",HeartDisplayMouseOut);
+    }
+    else if (window.detachEvent)
+    {
+        canvasMidNode.detachEvent("click",addImgFav);
+        midImgShowDiv.detachEvent("mouseover",heartDisplayOnHover);
+        midImgShowDiv.detachEvent("mouseout",HeartDisplayMouseOut);
+    }
+
 }
-function getId()
+function getMidNode()
 {
     return document.querySelector(".addOnloadStyleZDexTwo");
-    // let id = "cImag"+slideIndex;
-    // return id;
 }
-function openNewWidow(id)
+function getMidCanvasNode(midId)
 {
-    let newWindowHref;
+    let canvasMidId=midId+"-canvas";
+    return document.getElementById(canvasMidId);
+}
+
+function openNewWindow()
+{
+    let id = getMidNode().id;
+    let newWindowHref = "";
     if (id=="cImag1"){newWindowHref="cImag01.html";}
     else if (id=="cImag2"){newWindowHref="cImag02.html";}
     else if (id=="cImag3"){newWindowHref="cImag03.html";}
     else if (id=="cImag4"){newWindowHref="cImag04.html";}
     else if (id=="cImag5"){newWindowHref="cImag05.html";}
-    let newWindow=window.open(newWindowHref);
-
-    newWindow.close=function(){newWindow.opener.alert("abc");}
-   
-    //if (newWindow.closed){newWindow.opener.alert("abc");}
+    if(windowObjectReference == null || windowObjectReference.closed){
+        windowObjectReference = window.open(newWindowHref,
+        "BestViewer", "resizable,scrollbars,status");
+        
+        windowObjectReference.postMessage("hello there!", "*");
+      }else {
+          windowObjectReference.focus();
+      };
     
 }
 
@@ -220,11 +281,112 @@ function addFunctionToSingle(checkResult,copyNode,favBan)
     if (!checkResult)
     {
         favBan.appendChild(copyNode);
-        copyNode.addEventListener("click",function foo(){removeImgFav(copyNode.id)});
+        if (window.addEventListener)
+        {
+            copyNode.addEventListener("click",function foo(){removeImgFav(copyNode.id)});
+        }
+        else if(window.attachEvent)
+        {
+            copyNode.attachEvent("click",function foo(){removeImgFav(copyNode.id)});
+        }
     }
     else
     {
         window.alert("this picture has been added");
     }
 }
+function drawHeart(canvasId)
+{
+        canvas = document.getElementById(canvasId);
+        let context = canvas.getContext("2d");
+        let w = 15, h = 15;
+        context.strokeStyle = "#FFFFFF";
+        context.strokeWeight = 5;
+        context.shadowOffsetX = 2.0;
+        context.shadowOffsetY = 2.0;
+        context.lineWidth = 2.0;
+        let d = Math.min(w, h);
+        let k = 10;
+        context.moveTo(k, k + d / 4);
+        context.quadraticCurveTo(k, k, k + d / 4, k);
+        context.quadraticCurveTo(k + d / 2, k, k + d / 2, k + d / 4);
+        context.quadraticCurveTo(k + d / 2, k, k + d * 3/4, k);
+        context.quadraticCurveTo(k + d, k, k + d, k + d / 4);
+        context.quadraticCurveTo(k + d, k + d / 2, k + d * 3/4, k + d * 3/4);
+        context.lineTo(k + d / 2, k + d);
+        context.lineTo(k + d / 4, k + d * 3/4);
+        context.quadraticCurveTo(k, k + d / 2, k, k + d / 4);
+        context.stroke();
+}
+function fillHeart(canvasId)
+{
+    canvas = document.getElementById(canvasId);
+    let context = canvas.getContext("2d");
+    context.fillStyle = "#FF0000";
+    context.fill();
+   // context.strokeStyle = "#000000";
+}
+function removeFillHeart(canvasId)
+{
+    canvas = document.getElementById(canvasId);
+    let context = canvas.getContext("2d");
+    context.clearRect(0,0,30,30);
+    drawHeart(canvasId);
+   // context.strokeStyle = "#FFFFFF";
+}
+function HeartDisplayOnload(midCavSpanId)
+{
+    let heart=document.getElementById(midCavSpanId);
+    heart.classList.add("addHearDisplaynone");
+    zoomNoteOnload();
+}
+function heartDisplayOnHover()
+{
+    let heart=document.getElementById(getHeartSpanId());
+    heart.classList.remove("addHearDisplaynone");
+    zoomNoteShow();
+}
+function HeartDisplayMouseOut()
+{
+    let heart=document.getElementById(getHeartSpanId());
+    heart.classList.add("addHearDisplaynone");
+    zoomNoteOnload();
+}
+function getHeartSpanId()
+{
+    let midImgShowDiv=getMidNode();
+    let id =midImgShowDiv.getAttribute("id");
+    let midCavSpanId=id+"-canvas-span";
+    return midCavSpanId;
+}
+function zoomNoteOnload()
+{
+    let zoomNode=document.getElementById("zoomImgNoteId");
+    zoomNode.classList.add("addZoomNoteOnload");
+    zoomNode.classList.remove("addZoomNoteShow");
+}
+function zoomNoteShow()
+{
+    let zoomNode=document.getElementById("zoomImgNoteId");
+    zoomNode.classList.add("addZoomNoteShow"); 
+    zoomNode.classList.remove("addZoomNoteOnload");  
+}
+
+function receiveMessage(event)
+{
+  // Do we trust the sender of this message?  (might be
+  // different from what we originally opened, for example).
+  if (event.data === 0)
+    return;
+  addImgFav(event.data);
+}
+if (window.addEventListener)
+{
+    window.addEventListener("message", receiveMessage, false);
+}
+else if (window.attachEvent)
+{
+    window.attachEvent("message", receiveMessage, false);
+}
+
 //#endregion
